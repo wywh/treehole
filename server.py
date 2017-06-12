560b45fcbc7b94c5e3ea19e9b4a692b45e5ba393
Too-Naive <sometimes.naive@hotmail.com>
2017-06-13 07:47:45 +0800

    Improve Experience

--- server.py
+++ server.py
@@ -10,7 +10,6 @@ from config import botToken,sqlhost,sqlport,sqluser,sqlpwd,sqlname
 import sys
 from mysqlmodule import mysqlModule as mm
 import datetime
-import re
 
 bot = None
 
@@ -31,7 +30,7 @@ def onMessage(msg):
 
 	MESSAGE_PEAR_DAY = 3
 	MESSAGE_PEAR_CYCLE = 60*60*24
-	MESSAGE_COLD_DOWN_CYCLE = 60*5
+	MESSAGE_COOL_DOWN_CYCLE = 60*5
 
 	content_type, chat_type, chat_id = telepot.glance(msg)
 	if chat_type != 'private' or content_type!= 'text':
@@ -39,7 +38,7 @@ def onMessage(msg):
 	nowtimestamp = datetime.datetime.now()
 	sql = mm(sqlhost,sqlport,sqluser,sqlpwd,sqlname)
 	result = sql.query("SELECT * FROM `users` WHERE `user_id` = %d"%chat_id)
-	if not result and msg['text'] == '/ACCEPT':
+	if not result and msg['text'] == '/accept':
 		sql.execute('''
 			INSERT INTO `users` 
 			(`user_id`,`messageCycleTS`,`lastMessage`) 
@@ -53,25 +52,26 @@ def onMessage(msg):
 		sql.close()
 		bot.sendMessage(chat_id,'Congratulations! You can use this bot now.')
 		return
-	if not result and (msg['text'] == '/license' or msg['text'] == '/start'):
-		bot.sendMessage(chat_id,'We guarantee that we will send a message in a completely anonymous way.The bot will only record your '+
+	if msg['text'] == '/license' or msg['text'] == '/start':
+		bot.sendMessage(chat_id,'Bot license agreement:\n'+
+			'We guarantee that we will send a message in a completely anonymous way.The bot will only record your '+
 			'id and prevent spam from being sent. All messages you send to the bot are '
-			'not guaranteed to be published, and we will review your post anonymously.'+
-			'Bot code in github : https://github.com/Too-Naive/treehole'+
+			'not guaranteed to be published, and we will review your post anonymously.\n'+
+			'Bot code in github : https://github.com/Too-Naive/treeholn'+
 			'Author : @stdssr')
 		return
 	if not result:
-		bot.sendMessage(chat_id,"PLEASE SEND /ACCEPT TO ACCEPT OUR LICENSES AGREEMENT /license")
+		bot.sendMessage(chat_id,"PLEASE SEND /accept TO ACCEPT OUR LICENSES AGREEMENT /license")
 		sql.close()
 		return
 	if result[0][2] == 1:
 		bot.sendMessage(chat_id,"YOU HAS BEEN BLOCKED, PLEASE CONTACT ADMINISTRATOR")
 		sql.close()
 		return
-	elif datetimeFromNow(result[0][5]) > MESSAGE_COLD_DOWN_CYCLE:
+	elif datetimeFromNow(result[0][5]) > MESSAGE_COOL_DOWN_CYCLE:
 		if datetimeFromNow(result[0][4]) > MESSAGE_PEAR_CYCLE:
 			sql.execute("UPDATE `users` SET `messageCycleTS` = CURRENT_TIMESTAMP, `messagesInCycle` = 1, `lastMessage` = CURRENT_TIMESTAMP WHERE `user_id` = %d"%chat_id)
-		if result[0][6] < MESSAGE_PEAR_DAY:
+		if result[0][6] <= MESSAGE_PEAR_DAY:
 			sql.execute("UPDATE `users` SET `messagesInCycle` = %d, `lastMessage` = CURRENT_TIMESTAMP WHERE `user_id` = %d"%(result[0][6]+1,chat_id))
 		else:
 			if result[0][6] >= MESSAGE_PEAR_DAY:
@@ -79,11 +79,11 @@ def onMessage(msg):
 			sql.close()
 			return
 		result = sql.query("SELECT `user_id` FROM `users` WHERE `isForwardTarget` = 1")
-		sendMessage2Group(list(x[0] for x in result),msg['text'])
 		bot.sendMessage(chat_id,'Send successfully, Please waiting for review')
+		sendMessage2Group(list(x[0] for x in result),msg['text'])
 		sql.close()
 	else: 
-		bot.sendMessage(chat_id,'Portal running hot! Unsafe to send messages. Estimated time to cooldown: %d seconds'%calctime(result[0][5],nowtimestamp))
+		bot.sendMessage(chat_id,'Portal running hot! Unsafe to send messages. Estimated time to cooldown: %d seconds'%calctime(result[0][5]+datetime.timedelta(seconds=MESSAGE_COOL_DOWN_CYCLE),nowtimestamp))
 		sql.close()
 
 
@@ -92,10 +92,11 @@ sec = [5,10,20,30,60,120,180,240,300]
 
 def calctime(timestamp,nowatime):
 	global sec
-	dec = 300-datetimeFromNowEx(nowatime,timestamp)
+	dec = datetimeFromNowEx(timestamp,nowatime)
 	for x in sec:
 		if dec <= x:
 			return x
+	return dec
 
 def main():
 	global bot
