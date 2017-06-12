08b9c765a1be7b09c5fda816dcbd821d275e5f5b
Too-Naive <sometimes.naive@hotmail.com>
2017-06-12 19:30:01 +0800

    Add notice message

--- server.py
+++ server.py
@@ -40,11 +40,22 @@ def onMessage(msg):
 	sql = mm(sqlhost,sqlport,sqluser,sqlpwd,sqlname)
 	result = sql.query("SELECT * FROM `users` WHERE `user_id` = %d"%chat_id)
 	if not result and msg['text'] == '/ACCEPT':
+		bot.sendMessage('Congratulations! You can use this bot now.')
 		sql.execute("INSERT INTO `users` (`user_id`) VALUES (%d)"%chat_id)
 		sql.close()
 		return
+	if not result and msg['text'] == '/license':
+		bot.sendMessage('''
+We guarantee that we will send a message in a completely anonymous way. 
+The bot will only record your id and prevent spam from being sent. 
+All messages you send to the bot are not guaranteed to be published, 
+and we will review your post anonymously.
+
+Bot code in github : [github](https://github.com/Too-Naive/treehole)
+Author : @stdssr
+			''')
 	if not result:
-		bot.sendMessage(chat_id,"PLEASE SEND /ACCEPT TO ACCEPT OUR LICENSES AGREEMENT")
+		bot.sendMessage(chat_id,"PLEASE SEND /ACCEPT TO ACCEPT OUR LICENSES AGREEMENT /license")
 		sql.close()
 		return
 	if result[0][2] == 1:
@@ -57,6 +68,10 @@ def onMessage(msg):
 		elif result[0][6] < MESSAGE_PEAR_DAY:
 			sql.execute("UPDATE `users` SET `messagesInCycle` = %d WHERE `user_id` = %d"%(result[0][6],chat_id))
 		else:
+			if result[0][6] >= MESSAGE_PEAR_DAY:
+				bot.sendMessage('Portal burn out! It may take significant time for the Portal to reset.')
+			else:
+				bot.sendMessage('Please wait sometime to cold down yourself.')
 			sql.close()
 			return
 		result = sql.query("SELECT `user_id` FROM `users` WHERE `isForwardTarget` = 1")
